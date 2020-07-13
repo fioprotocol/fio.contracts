4b1804e5f31044bd5bc29889ac4dccc9f2fd22db
Casey Gardiner <casey@dapix.io>
2020-07-13 14:34:31 -0600

    Remove build directory

--- build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/casey/Desktop/fio.contracts/tests
-# Build directory: /Users/casey/Desktop/fio.contracts/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
