c8593ddd491aa03665967f3d033730d9c4942edf
Casey Gardiner <casey@dapix.io>
2020-07-28 15:36:23 -0600

    Add support for calls

--- contracts/fio.system/include/fio.system/native.hpp
+++ contracts/fio.system/include/fio.system/native.hpp
@@ -261,6 +261,10 @@ namespace eosiosystem {
         using updateauth_action = eosio::action_wrapper<"updateauth"_n, &native::updateauth>;
         using deleteauth_action = eosio::action_wrapper<"deleteauth"_n, &native::deleteauth>;
         using linkauth_action = eosio::action_wrapper<"linkauth"_n, &native::linkauth>;
+
+        using adddaction_action = eosio::action_wrapper<"addaction"_n, &native::addaction>;
+        using remaction_action = eosio::action_wrapper<"remaction"_n, &native::remaction>;
+
         using unlinkauth_action = eosio::action_wrapper<"unlinkauth"_n, &native::unlinkauth>;
         using canceldelay_action = eosio::action_wrapper<"canceldelay"_n, &native::canceldelay>;
         using setcode_action = eosio::action_wrapper<"setcode"_n, &native::setcode>;
