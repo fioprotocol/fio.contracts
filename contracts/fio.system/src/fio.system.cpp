9e75f4acdfca4a7e2a7e50eb0d54e6956322fdce
adsorptionenthalpy <androulidakisa@gmail.com>
2020-10-23 11:44:21 -0600

    enable assert

--- contracts/fio.system/src/fio.system.cpp
+++ contracts/fio.system/src/fio.system.cpp
@@ -147,7 +147,7 @@ namespace eosiosystem {
 
         set_resource_limits(newact.value, INITIALACCOUNTRAM, -1, -1);
 
-        //fio_400_assert(transaction_size() <= MAX_TRX_SIZE, "transaction_size", std::to_string(transaction_size()),
+        fio_400_assert(transaction_size() <= MAX_TRX_SIZE, "transaction_size", std::to_string(transaction_size()),
           "Transaction is too large", ErrorTransactionTooLarge);
 
     }
