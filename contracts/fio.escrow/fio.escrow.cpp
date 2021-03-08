3c9081e955c43056073d128f24a44e2e3945241d
Thomas <tvle83@gmail.com>
2021-03-08 00:03:24 -0600

    added notes to all the actions on the basic steps required. Updated abi file with new parameters for each action. used some examples to add some asserts to the listdomain action

--- contracts/fio.escrow/fio.escrow.cpp
+++ contracts/fio.escrow/fio.escrow.cpp
@@ -1,20 +1,81 @@
-//
-// Created by tvle83 on 20-Jan-21.
-//
+/** FioEscrow implementation file
+ *  Description: FioEscrow is the smart contract that allows the sell and purchasing of domains
+ *  @author Thomas Le (BlockSmith)
+ *  @modifedby
+ *  @file fio.escrow.cpp
+ *  @license
+ */
+
 #include "fio.escrow.hpp"
+#include <fio.address/fio.address.hpp>
 
 namespace fioio {
 
     class [[eosio::contract("FioEscrow")]] FioEscrow : public eosio::contract {
+    private:
+        domainsales_table domainsales;
+        domains_table domains;
     public:
         using contract::contract;
 
         FioEscrow(name s, name code, datastream<const char *> ds) :
-                contract(s, code, ds) {
+                contract(s, code, ds),
+                domains(AddressContract, AddressContract.value),
+                domainsales(_self, _self.value) {
+        }
+
+        /***********
+        * This action will list a fio domain for sale
+        * @param fio_domain this is the fio domain to be sold.
+        * @param sale_price this is the amount of FIO the seller wants for the domain
+        * @param tpid  this is the fio address of the owner of the domain.
+        * @param actor this is the fio account that has sent this transaction.
+        */
+        [[eosio::action]]
+        void listdomain(const string &fio_domain, const int64_t &sale_price,
+                        const string &tpid, const name &actor){
+
+            // Steps to take in this action:
+            // -- Verify the actor owns the domain
+            // -- Verify domain will not expire in 90 days
+            // -- Verify the sale price is valid
+            // *** not sure if i should write to the table first or transfer the domain first
+            // -- require the seller to pay domainxfer fee to the chain
+            // * require the seller to send domainxfer fee to fio.escrow (to cover the instance of a cancel OR to cover the xfer in the event of a sale)
+            // -- transfer domain to fio.escrow
+            // -- write the domain listing to the table
+            // * this poses a problem because the xfer fee can change between the time it is listed and it gets cancelled or sold
+            // this can be remedied by not collecting that fee up front and have it paid out of the sale price or when the seller cancels it'll cost the xfer fee
+            // or we can collect the xfer fee at the time of listing and save that to the table and if the fee has changed just charge or refund the difference
+
+            require_auth(actor);
+            fio_400_assert(validateTPIDFormat(tpid), "tpid", tpid,
+                           "TPID must be empty or valid FIO address",
+                           ErrorPubKeyValid);
+
+            fio_400_assert(sale_price > 0 && sale_price > 0, "sale_price", std::to_string(sale_price),
+                           "Invalid sale_price value", ErrorInvalidAmount);
+
+            const uint128_t domainHash = string_to_uint128_hash(fio_domain.c_str());
+
+            auto domainsbyname = domains.get_index<"byname"_n>();
+            auto domains_iter = domainsbyname.find(domainHash);
+
+            fio_400_assert(domains_iter != domainsbyname.end(), "fio_domain", fio_domain,
+                           "FIO domain not found", ErrorDomainNotRegistered);
+
+            const string response_string = string("\"status\":\"OK\"}");
+
+            send_response(response_string.c_str());
         }
 
         [[eosio::action]]
-        void listdomain(const name &actor){
+        void cxlistdomain(const name &actor, const uint64_t &domainlist_id){
+            // Steps to take in this action:
+            // -- Verify the actor is on the listing
+            // -- transfer domain to actor
+            // -- remove listing from table
+
             require_auth(actor);
 
             const string response_string = string("{\"status\": \"OK\"}");
@@ -23,23 +84,21 @@ namespace fioio {
         }
 
         [[eosio::action]]
-        void cxlistdomain(const name &actor){
+        void buydomain(const name &actor, const uint64_t &domainlist_id){
+            // Steps to take in this action:
+            // -- Verify the actor is on the listing
+            // -- retrieve FIO for the sale price
+            // -- transfer domain to actor
+            // -- divvy up the fees between marketplace, seller and bp fees
+            // -- remove listing from table
+
             require_auth(actor);
 
             const string response_string = string("{\"status\": \"OK\"}");
 
             send_response(response_string.c_str());
         }
-
-        [[eosio::action]]
-        void buydomain(const name &actor){
-            require_auth(actor);
-
-            const string response_string = string("{\"status\": \"OK\"}");
-
-            send_response(response_string.c_str());
-        }
-    };
+    }; // class FioEscrow
 
     EOSIO_DISPATCH(FioEscrow, (listdomain)(cxlistdomain)(buydomain))
 }
--- contracts/fio.escrow/fio.escrow.hpp
+++ contracts/fio.escrow/fio.escrow.hpp
@@ -1,6 +1,10 @@
-//
-// Created by tvle8 on 20-Jan-21.
-//
+/** FioEscrow header file
+ *  Description: FioEscrow is the smart contract that allows the sell and purchasing of domains
+ *  @author Thomas Le (BlockSmith)
+ *  @modifedby
+ *  @file fio.escrow.hpp
+ *  @license
+ */
 
 #ifndef FIO_CONTRACTS_FIO_ESCROW_H
 #define FIO_CONTRACTS_FIO_ESCROW_H
@@ -9,5 +13,30 @@
 #include <string>
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/asset.hpp>
+
+using std::string;
+
+namespace fioio {
+
+    // table will store all the domains that are for sale
+    // it needs the name of the domain, the seller and the amount the seller wants in FIO
+    struct [[eosio::action]] domainsale {
+
+        uint64_t id = 0;
+        uint64_t owner;
+        uint128_t ownerhash = 0;
+        string domain = nullptr;
+        uint128_t domainhash = 0;
+        asset sale_price;
+
+        // primary_key is required to store structure in multi_index table
+        uint64_t primary_key() const { return id; }
+
+        EOSLIB_SERIALIZE(domainsale, (id)(owner)(ownerhash)(domain)(domainhash)(sale_price))
+    };
+
+    typedef multi_index<"domainsales"_n, domainsale>
+            domainsales_table;
+}
 
 #endif //FIO_CONTRACTS_FIO_ESCROW_H
