7000e8d8863ea5e4f34b50052738868283ed6d60
Ed Rotthoff <ed@fioprotocol.io>
2021-05-01 10:27:50 -0600

    make fio_address to be optional
    make fio address parameter to be optional

--- contracts/fio.treasury/fio.treasury.cpp
+++ contracts/fio.treasury/fio.treasury.cpp
@@ -361,8 +361,8 @@ public:
         void bprewdupdate(const uint64_t &amount) {
 
                 eosio_assert((has_auth(AddressContract) || has_auth(TokenContract) || has_auth(TREASURYACCOUNT) ||
-                             has_auth(REQOBTACCOUNT) || has_auth(SYSTEMACCOUNT) || has_auth(FeeContract)),
-                             "missing required authority of fio.address, fio.treasury, fio.fee, fio.token, eosio or fio.reqobt");
+                             has_auth(STAKINGACCOUNT) ||  has_auth(REQOBTACCOUNT) || has_auth(SYSTEMACCOUNT) || has_auth(FeeContract)),
+                             "missing required authority of fio.address, fio.treasury, fio.fee, fio.token, fio.stakng, eosio or fio.reqobt");
 
                 bprewards.set(bprewards.exists() ? bpreward{bprewards.get().rewards + amount} : bpreward{amount}, get_self());
         }
@@ -380,13 +380,11 @@ public:
         // @abi action
         [[eosio::action]]
         void fdtnrwdupdat(const uint64_t &amount) {
-
                 eosio_assert((has_auth(AddressContract) || has_auth(TokenContract) || has_auth(TREASURYACCOUNT) ||
-                             has_auth(REQOBTACCOUNT) || has_auth(SYSTEMACCOUNT) || has_auth(FeeContract)),
-                             "missing required authority of fio.address, fio.token, fio.fee, fio.treasury or fio.reqobt");
+                             has_auth(STAKINGACCOUNT) || has_auth(REQOBTACCOUNT) || has_auth(SYSTEMACCOUNT) || has_auth(FeeContract)),
+                             "missing required authority of fio.address, fio.token, fio.fee, fio.treasury, fio.staking, or fio.reqobt");
 
                 fdtnrewards.set(fdtnrewards.exists() ? fdtnreward{fdtnrewards.get().rewards + amount} : fdtnreward{amount}, get_self());
-
         }
 
 };     //class FIOTreasury
