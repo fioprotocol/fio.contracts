86ffe932d37af76d50c9b266a92124baca2c0966
adsorptionenthalpy <androulidakisa@gmail.com>
2021-09-02 08:26:18 -0600

    Add extra binary extended properties to nft data structure

--- contracts/fio.address/fio.address.hpp
+++ contracts/fio.address/fio.address.hpp
@@ -10,6 +10,8 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/singleton.hpp>
 #include <eosiolib/asset.hpp>
+#include <eosiolib/binary_extension.hpp>
+
 #include <string>
 
 using std::string;
@@ -127,6 +129,8 @@ namespace fioio {
       string hash;
       uint128_t hash_index;
       string metadata;
+      eosio::binary_extension<uint64_t> property1;
+      eosio::binary_extension<uint128_t> property2;
 
       uint64_t primary_key() const { return id; }
       uint128_t by_address() const { return fio_address_hash; }
