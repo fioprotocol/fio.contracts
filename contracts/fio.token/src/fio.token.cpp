85416cb7495ab6a1f6785e43346ece1b90b0d7ed
adsorptionenthalpy <androulidakisa@gmail.com>
2021-11-16 08:47:58 -0700

    Correction: use AND not OR

--- contracts/fio.token/src/fio.token.cpp
+++ contracts/fio.token/src/fio.token.cpp
@@ -98,7 +98,7 @@ namespace eosio {
         const asset my_balance = eosio::token::get_balance("fio.token"_n, actor, FIOSYMBOL.code());
         const int64_t uamount = genesislockedamount + generallockedamount + my_balance.amount;
 
-        fio_400_assert(uamount > 0 || uamount - genesislockedamount >= qty.amount, "actor", to_string(actor.value),
+        fio_400_assert(uamount > 0 && uamount - genesislockedamount >= qty.amount, "actor", to_string(actor.value),
                        "Insufficient balance",
                        ErrorInsufficientUnlockedFunds);
 
